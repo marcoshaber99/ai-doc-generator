0128d6d6989c6722a7734815c50dbc13489d68b6
Marco Haber <marcoshaber99@gmail.com>
2024-11-19 18:14:37 +0200

    setup clerk, vercel pg db, resumes table

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,17 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Geist } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { ClerkProvider } from "@clerk/nextjs";
+const geistSans = Geist({
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: {
-    template: "%s - AI Resume Builder",
-    absolute: "AI Resume Builder",
+    template: "%s - Lexos",
+    absolute: "Lexos",
   },
-  description:
-    "AI Resume Builder is the easiest way to create a professional resume that will help you land your dream job.",
+  description: "Lexos is the easiest way to generate content.",
 };
 
 export default function RootLayout({
@@ -19,8 +20,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={geistSans.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
