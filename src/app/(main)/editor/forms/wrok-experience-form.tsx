20c4843321824564f68426944aa12a6d91232180
Marco Haber <marcoshaber99@gmail.com>
2024-12-16 19:47:42 +0200

    start work experience form

---
+++ src/app/(main)/editor/forms/wrok-experience-form.tsx
@@ -0,0 +1,12 @@
+import { EditorFormProps } from "@/lib/types";
+
+export default function WorkExperienceForm({
+  resumeData,
+  setResumeData,
+}: EditorFormProps) {
+  return (
+    <div>
+      <h1>Work Experience</h1>
+    </div>
+  );
+}
